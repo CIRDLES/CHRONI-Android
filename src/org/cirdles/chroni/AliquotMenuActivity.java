fdc2252bf2fb003cdcb519878669dd88df25772b
Gabrielle <gabriellecozart@yahoo.com>
2015-07-05 19:19:49 -0400

    v0.8.0 - Stable Release With Support for Horizontal Orientation

--- src/org/cirdles/chroni/AliquotMenuActivity.java
+++ src/org/cirdles/chroni/AliquotMenuActivity.java
@@ -53,7 +53,7 @@ public class AliquotMenuActivity extends Activity {
 	    super.onCreate(savedInstanceState);
 	    setTheme(android.R.style.Theme_Holo);
 	    setContentView(R.layout.aliquot_select);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
 
         RelativeLayout aliquotMenuLayout =(RelativeLayout)findViewById(R.id.aliquotSelectBackground);
 
