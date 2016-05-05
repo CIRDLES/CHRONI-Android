090e49499aa3896b3a35a6cd97e51f1ec607d015
Gabrielle Cozart <gabriellecozart@Gabrielles-MBP.sc.rr.com>
2016-05-05 17:14:34 -0400

    Updating License

--- src/main/java/org/cirdles/chroni/AboutActivity.java
+++ src/main/java/org/cirdles/chroni/AboutActivity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015=6 CIRDLES.
+ * Copyright 2016 CIRDLES.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
